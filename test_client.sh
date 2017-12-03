b966f9f7a35a371c3c2a06d92f219dd1128867e8
jnayak1 <jdn4ae@virginia.edu>
2017-12-02 22:29:51 -0500

    Collect all results

--- test_client.sh
+++ test_client.sh
@@ -8,59 +8,59 @@ touch results.txt
 
 # small pattern increase text size
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=8 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=16 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=32 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=64 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=128 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=512 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=1024 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=2048 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=4096 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=8192 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 
 # small text, increase patter size
 MAX_PATTERN_LENGTH=1 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern1.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern1.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=4 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern4.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern4.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=5 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern5.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern5.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=8 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern8.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern8.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=16 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern16.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern16.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=64 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern64.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern64.txt >> results.txt 2>&1
 sleep $sleep_time
 MAX_PATTERN_LENGTH=128 MAX_TEXT_LENGTH=256 make
-./a.out 2345 $host test_patterns/pattern128.txt >> results.txt
+./a.out 2345 $host test_patterns/pattern128.txt >> results.txt 2>&1
 
 
